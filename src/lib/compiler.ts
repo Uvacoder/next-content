26bc8bce48318db6ff8758048e4182ee67077855
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-29 20:31:14 +0300

    feat: customizing mdx and swc options

--- src/lib/compiler.ts
+++ src/lib/compiler.ts
@@ -1,35 +1,56 @@
-import MDXCompiler from "@mdx-js/mdx";
-import swc, { Options, Output } from "@swc/core";
+import MDXCompiler from '@mdx-js/mdx';
+import * as swc from '@swc/core';
+import type { Compiler, Pluggable } from 'unified';
 
-const swcOptions: Options = {
+const swcOptions: swc.Options = {
   jsc: {
     parser: {
-      syntax: "ecmascript",
+      syntax: 'ecmascript',
       jsx: true,
     },
-    target: "es2020",
+    target: 'es2020',
     loose: true,
   },
-  minify: process.env.NODE_ENV === "production" ? true : false,
+  minify: process.env.NODE_ENV === 'production' ? true : false,
 };
 
 const mdxOptions = { skipExport: true };
 
 export interface CompileOptions {
-  mdxOptions?: any;
+  /**
+   * These options are passed to the MDX compiler.
+   * See [the MDX docs.](https://github.com/mdx-js/mdx/blob/master/packages/mdx/index.js).
+   */
+  mdxOptions?: {
+    remarkPlugins?: Pluggable[];
+    rehypePlugins?: Pluggable[];
+    hastPlugins?: Pluggable[];
+    compilers?: Compiler[];
+    filepath?: string;
+  };
+
+  swcOptions?: swc.Options;
 }
 
-export type CompileResult = Output["code"];
+export type CompileOutput = string;
 
-export type CompileFunction = (
+export type Compile = (
   mdxSource: string,
   options?: CompileOptions
-) => Promise<CompileResult>;
+) => Promise<CompileOutput>;
 
-export const compile: CompileFunction = async (mdxSource) => {
-  const mdxCompiledJsx = await MDXCompiler(mdxSource, mdxOptions);
+export const compile: Compile = async (mdxSource, options) => {
+  const mdxCompiledJsx = await MDXCompiler(mdxSource, {
+    // can't overwrite the default options
+    ...options?.mdxOptions,
+    ...mdxOptions,
+  });
 
-  const { code } = await swc.transform(mdxCompiledJsx, swcOptions);
+  const { code } = await swc.transform(mdxCompiledJsx, {
+    // can overwrite the default options
+    ...swcOptions,
+    ...options?.swcOptions,
+  });
 
   return code;
 };
