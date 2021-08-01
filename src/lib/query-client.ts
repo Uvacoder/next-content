a0ed0598869e690049324945589450695e93fd78
healthpackdev <yasinkadirkilic@gmail.com>
2021-08-01 19:36:40 +0300

    chore: remove warning

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -2,26 +2,11 @@ import { Query } from './query';
 import { FetchResult } from './query-server';
 
 export class QueryClient<T> extends Query<FetchResult<T>> {
-  private contents: FetchResult<T>[];
-
   constructor(contents: FetchResult<T>[]) {
-    if (contents.some((content) => !content.text))
-      console.error(
-        'the text field in content is required, please enable text option in content.'
-      );
-    super(
-      contents.map(({ data, text, slug }) => ({
-        data,
-        slug,
-        text,
-        code: undefined,
-      }))
-    );
-    this.contents = contents;
+    super(contents);
   }
 
   public fetch() {
-    // const contents = this.apply();
     return this.apply();
   }
 }
